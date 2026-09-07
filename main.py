9d53fff48fe5d5dad0ac37f52b018dead54422d3
homelab-black <homelab.black@gmail.com>
2026-09-07 12:29:10 +0900

    各ソースコードにライセンス情報を明記、README.md 追記

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
+# Copyright (C) 2026 homelab-black
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://gnu.org>.
+
 import requests
 import shutil
 import signal
 import sys
 import time
+from pathlib import Path
+
 from mpcbe_monitor import MpcbeHandler
 from lcd_controller import LcdController
-from pathlib import Path
 
 def main():
     """ メイン処理 """
--- mpcbe_monitor.py
+++ mpcbe_monitor.py
@@ -1,16 +1,31 @@
-import io
-import requests
-import psutil
-import math
-import music_tag
-import os
+# Copyright (C) 2026 homelab-black
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://gnu.org>.
+
 import hashlib
+import io
+import math
+import os
 import random
 import re
-import requests
-from itertools import chain
 from dataclasses import dataclass
+from itertools import chain
 from pathlib import Path
+
+import music_tag
+import psutil
+import requests
 from bs4 import BeautifulSoup
 from PIL import Image, ImageDraw, ImageFont
 
